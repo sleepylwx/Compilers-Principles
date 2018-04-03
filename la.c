f3e269da13707dbd8896fd47ae7bd13280dedd3b
lwx <sleepylwx@gmail.com>
2018-04-03 21:36:39 +0800

    add variable table

--- la.c
+++ la.c
@@ -8,14 +8,54 @@
 /*
 keep source code every line including less word than 65536
 */
+#define IDENTIFIER 1
+#define CONSTENT 2
+#define RESERVEDWORD 3
+#define OPERATOR 4
+#define BOUND 5
+/**
+1.标识符 type = 1
+2.常量   type = 2 *
+3.保留字 if else while for int float string  type = 3
+4.运算符 + - * / = += -= *= /= == <= >= < >  type = 4 *()
+5.界符 ; SPACE  type = 5
+6.区域限定符 {}
+**/
 
 /**
-1.标识符
-2.常量
-3.保留字 if else while for int float string
-4.运算符 + - * / = += -= *= /= == <= >= < >
-5.界符 ; SPACE
-**/
+
+if else while for 后面()的完整性判定放语法分析
+多个语句的语句块区域限定符{}的完整性判定放语法分析
+
+
+ **/
+
+struct Word{
+
+
+    int type;
+    char* word;
+    int length;
+
+};
+struct Error{
+
+
+    int type;
+    char* description;
+    int lineNumber;
+
+};
+
+struct String{
+
+    char* str;
+    int length;
+
+};
+struct Word* wordTable;
+int wordTableLimit;
+int wordTableCurrent;
 
 char map[7][7] = {"if","else","while","for","int","float","string"};
 int lineCounter = 1;
@@ -95,6 +135,41 @@ void enlargeArray(char*p,int* num){
 
 }
 
+void enLargeWordTable(struct Word* p,int* num){
+
+
+    struct Word* temp = (struct Word*)malloc(2*sizeof(struct Word)*(*num));
+    int i;
+    for(i = 0; i < *num ; ++i ){
+
+
+        temp[i].type = p[i].type;
+        temp[i].word = p[i].word;
+        temp[i].length = p[i].length;
+    }
+
+    free(p);
+    p = temp;
+
+    (*num) *= 2;
+}
+
+int isWordTableFull(){
+
+
+    return wordTableCurrent >= wordTableLimit ? 1 : 0;
+
+}
+
+void prepareWordTable(){
+
+
+    if(isWordTableFull()){
+
+        enLargeWordTable(wordTable,&wordTableLimit);
+    }
+
+}
 void variableNameError(char* p){
 
 
@@ -147,6 +222,42 @@ int min(int x,int y){
     return x > y ? y : x;
 
 }
+
+int encodeTypeId(int kind){
+
+
+    return kind;
+
+}
+
+void addWordTable(int type, char* str){
+
+    int i;
+    for(i = 0; str[i] != 0; ++i){
+
+
+    };
+
+    prepareWordTable();
+    wordTable[wordTableCurrent].type = encodeTypeId(type);
+    wordTable[wordTableCurrent].word = (char*)malloc(sizeof(char)*i);
+    strcat(wordTable[wordTableCurrent].word,str);
+    wordTable[wordTableCurrent].length = i;
+    ++wordTableCurrent;
+
+}
+int isRealFrontier(char ch){
+
+
+    if(ch == ';'){
+
+        return 1;
+    }
+
+    return 0;
+}
+
+
 void analyse(char* code){
 
     int i;
@@ -155,7 +266,13 @@ void analyse(char* code){
 
         if(isFrontier(code[i])){
 
+            if(isRealFrontier(code[i])){
 
+                char temp[2];
+                temp[0] = ';';
+                temp[1] = 0;
+                addWordTable(BOUND,temp);
+            }
             continue;
         }
 
@@ -184,8 +301,10 @@ void analyse(char* code){
             }
 
             if(status == 1){
-
+                /** 是保留字 **/
                 i = j - 1;
+                addWordTable(RESERVEDWORD,p);
+
                 //free(p);
                 continue;
             }
@@ -214,6 +333,8 @@ void analyse(char* code){
                 else{
 
                     //free(p);
+                    addWordTable(IDENTIFIER,p);
+
                     continue;
                 }
             }
@@ -225,108 +346,82 @@ void analyse(char* code){
             j = min(k,j);
             int status = 0;
 
+            int eNumber = 0;
+            int dotNumber = 0;
             for(k = i;k < j; ++k){
 
-
+                k = i+-k;
                 if(!isdigit(code[k])){
 
+                    if(eNumber == 0 && code[k] == 'e'){
 
+                        ++eNumber;
+                    }
+                    if(eNumber == 0 && dotNumber == 0 && code[k] == '.'){
+
+                        ++dotNumber;
+                    }
                     status = 1;
                     break;
                 }
             }
 
+            char temp[j - i + 1];
+            for(k = 0; k < j - i; ++k){
+
+
+                temp[k] = code[i+k];
+            }
+            temp[k] = 0;
             if(status == 1){
 
                 /**输出错误信息**/
-                char temp[j - i + 1];
-                for(k = 0; k < j - i; ++k){
 
-
-                    temp[k] = code[i+k];
-                }
-                temp[k] = 0;
                 constentDigitError(temp);
 
 
             }
             else{
 
+                addWordTable(CONSTENT,temp);
 
             }
             i = j - 1;
 
-//            char* p = (char*) malloc(256);
-//            int counter = 1;
-//            int num = 256;
-//            p[0] = code[i];
-//            int status = 0;
-//            for(j = i + 1;isStrNotEnd(code[j]); ++j){
-//
-//                if(isdigit(code[j])){
-//
-//                    if(num = counter){
-//
-//                        enlargeArray(p,&num);
-//
-//                    }
-//                    p[counter++] = code[j];
-//                    continue;
-//                }
-//                else if(isFrontier(code[j])){
-//
-//                    break;
-//                }
-//                else if(isalpha(code[j])){
-//
-//                    /**输出错误信息**/
-//
-//                    p[counter++] = code[j];
-//                    p[counter] = 0;
-//                    constentDigitError(p);
-//                    //找到下一个界符，继续词法分析
-//
-//                    i = findNextFrontier(code,j) - 1;
-//                    status = 1;
-//                    break;
-//
-//                }
-//
-//                else if(isOperator(code[j])){
-//
-//                    break;
-//
-//                }
-//            }
-//
-//            if(status == 0){
-//
-//                i = j-1;
-//            }
-//            else{
-//
-//
-//            }
-//
-//            free(p);
+
 
         }
         else if(isOperator(code[i])){
 
+
             if(!isStrNotEnd(code[i+1])){
 
+
+                char temp[2];
+                temp[0] = code[i];
+                temp[1] = 0;
+                addWordTable(OPERATOR,temp);
                 continue;
             }
             if(!isOperator(code[i+1])){
 
-
+                char temp[2];
+                temp[0] = code[i];
+                temp[1] = 0;
+                addWordTable(OPERATOR,temp);
                 continue;
             }
             int isSame = isSameOperator(code[i],code[i+1]);
 
             if(!isSame && (code[i] == '+' || code[i] == '-' || code[i] == '*' || code[i] == '/' || code[i] == '=' || code[i] == '<' || code[i] == '>') && code[i+1] == '='){
 
+                char temp[3];
+                temp[0] = code[i];
+                temp[1] = code[i+1];
+                temp[2] = 0;
+                addWordTable(OPERATOR,temp);
                 ++i;
+
                 continue;
             }
             if(!isSame){
@@ -341,7 +436,13 @@ void analyse(char* code){
             }
             if(code[i] == '+' || code[i] == '-' ){
 
+                char temp[3];
+                temp[0] = code[i];
+                temp[1] = code[i+1];
+                temp[2] = 0;
+                addWordTable(OPERATOR,temp);
                 ++i;
+
                 continue;
             }
             else{
@@ -356,6 +457,7 @@ void analyse(char* code){
         else if(isString(code[i])){
 
             int status = 0;
+
             for(j = i+1; isStrNotEnd(code[j]);++j){
 
                 if(code[j] == '"' && code[j-1] != '\\'){
@@ -373,10 +475,19 @@ void analyse(char* code){
             }
             else{
 
+                char temp[j-i];
+                int k;
+                for(k = 0 ; k < j - i; ++k){
+
+                    temp[k] = code[i+k+1];
+                }
+                temp[k] = 0;
+                addWordTable(CONSTENT,temp);
                 i = j;
                 continue;
             }
         }
+
 
 
 
@@ -405,6 +516,9 @@ int main(int argc,char** argv){
     char buffer[MAXLINE];
 
     int k;
+    wordTable = (struct Word*)malloc(sizeof(struct Word)*20);
+    wordTableCurrent = 0;
+    wordTableLimit = 20;
     while(fgets(buffer,MAXLINE,fp) != NULL){
 
 
@@ -418,7 +532,20 @@ int main(int argc,char** argv){
         analyse(buffer);
         ++lineCounter;
     }
+    putchar('\n');
+
+    for(k = 0 ; k < wordTableCurrent ; ++k) {
 
 
+        printf("[%d] : \"%s\"\n",wordTable[k].type,wordTable[k].word);
+    }
+
+
+    for(k = 0 ; k < wordTableCurrent ; ++k){
+
+
+        free(wordTable[k].word);
+    }
+    free(wordTable);
     return 0;
 }
